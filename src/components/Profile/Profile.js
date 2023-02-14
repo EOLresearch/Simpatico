2c112cda18cdcab4a1291b5f62e64c1071cc3341
mrjoemiller <joe@joemiller.co>
2023-02-14 11:00:13 -0500

    work started on profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -9,6 +9,17 @@ export default function Profile({ user }) {
 
   const firstName = user.displayName.split(" ")
 
+  function getAge(date) {
+    const today = new Date();
+    const bday = new Date(date);
+    let age = today.getFullYear() - bday.getFullYear();
+    let m = today.getMonth() - bday.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < bday.getDate())) {
+        age--;
+    }
+    return age;
+}
+
   return (
     <IconContext.Provider value={{ className: "react-icons-profile" }}>
       <div className='profile-container'>
@@ -20,10 +31,10 @@ export default function Profile({ user }) {
 
           <div className='profile-body'>
             <p><strong>Hello, {user.displayName}.</strong></p>
-            <p className='body'>You have been matched with 3 other users who have suffered a very similar loss. You can see their stories, and start a conversation in the "Matches" tab. </p>
+            <p className='body'>You have been matched with ::number of:: other users who have suffered a similar loss. You can see their stories, and start a conversation in the "Matches" tab. </p>
 
             <h4>My Details</h4>
-            <p className='details'>57, Male, NY<br />Lost Relationship: Partner</p>
+            <p className='details'>{getAge(user.birthDate)}, Male, NY<br />Lost Relationship: </p>
             <h4>My Story</h4>
             <p className='story'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
           </div>
