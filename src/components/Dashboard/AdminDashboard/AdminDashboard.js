a2655b872d8a7912a1ab39f2b1a0b745a36ed9ac
mrjoemiller <joe@joemiller.co>
2023-07-27 10:30:43 -0400

    filters started - may do this a bit differently than the original idea

--- src/components/Dashboard/AdminDashboard/AdminDashboard.js
+++ src/components/Dashboard/AdminDashboard/AdminDashboard.js
@@ -1,11 +1,15 @@
 import './admindashboard.css';
 import UserDatabase from './UserDatabase';
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 // import { IconContext } from "react-icons";
 
 export default function AdminDashboard({ firebase, fsUser, navHandler }) {
+  const [showAllUsers, setShowAllUsers] = useState(true)
+  const [showCauseMatches, setShowCauseMatches] = useState(false)
+  const [showKinshipMatches, setShowKinshipMatches] = useState(false)
+
 
   const firestore = firebase.firestore();
 
@@ -18,16 +22,37 @@ export default function AdminDashboard({ firebase, fsUser, navHandler }) {
   const usersRef = firestore.collection('users');
   const [users] = useCollectionData(usersRef);
 
-  return (
+  const subNavHandler = (renderCondition) => {
+    switch (renderCondition) {
+      case 'Cause':
+        setShowCauseMatches(true)
+        setShowKinshipMatches(false)
+        setShowAllUsers(false)
+        return
+      case 'Kinship':
+        setShowKinshipMatches(true)
+        setShowCauseMatches(false)
+        setShowAllUsers(false)
+        return
+      case 'All':
+        setShowAllUsers(true)
+        setShowCauseMatches(false)
+        setShowKinshipMatches(false)
+        return
+      default:
+        return
+    }
+  }
 
+  return (
     <div className='admin-dashboard-container'>
       <div className='admin-dashboard'>
         <div className='admin-dashboard-header'>
           <h1>Admin Dashboard</h1>
           <div className='admin-dashboard-nav'>
-            <button>All Users</button>
-            <button>Matching on Cause</button>
-            <button>Matching on Kinship</button>
+            <button onClick={e=>subNavHandler("All")}>All Users</button>
+            <button onClick={e=>subNavHandler("Cause")} >Matching on Cause</button>
+            <button onClick={e=>subNavHandler("Kinship")} >Matching on Kinship</button>
             <button>Matching on both</button>
             <button>No Match</button>
           </div>
@@ -36,6 +61,9 @@ export default function AdminDashboard({ firebase, fsUser, navHandler }) {
           <UserDatabase
             firestore={firestore}
             users={users}
+            showAllUsers={showAllUsers}
+            showCauseMatches={showCauseMatches}
+            showKinshipMatches={showKinshipMatches}
           />
         </div>
       </div>
--- src/components/Dashboard/AdminDashboard/UserDatabase.js
+++ src/components/Dashboard/AdminDashboard/UserDatabase.js
@@ -3,7 +3,7 @@ import UserCard from './UserCard'
 import './userdatabase.css'
 
 
-export default function UserDatabase({ firestore, users }) {
+export default function UserDatabase({ firestore, users, showAllUsers, showCauseMatches, showKinshipMatches }) {
   const [selectedUser, setSelectedUser] = useState()
   const [hovered, setHovered] = useState(false)
 
@@ -20,7 +20,7 @@ export default function UserDatabase({ firestore, users }) {
   const getMatchBy = (uid, userCause, userKinship, type) => {
     console.log(type)
     const unmatchedUsers = users.filter(user => user.simpaticoMatch === '')
-    
+
     if (type === 'cause') {
       const match = unmatchedUsers.find(user => user.cause === userCause && user.uid !== uid)
       if (!match) return alert('no match available')
@@ -34,7 +34,7 @@ export default function UserDatabase({ firestore, users }) {
       if (!match) return alert('no match available')
       setSelectedUser(match)
     } else if (type === 'none') {
-      const match = unmatchedUsers.find(user => user.cause !== userCause && user.kinship !== userKinship && user.uid !== uid) 
+      const match = unmatchedUsers.find(user => user.cause !== userCause && user.kinship !== userKinship && user.uid !== uid)
       if (!match) return alert('no match available')
       setSelectedUser(match)
       console.log(match)
@@ -55,7 +55,6 @@ export default function UserDatabase({ firestore, users }) {
     selectedRef.update({
       simpaticoMatch: useruid
     })
-
     setHovered(false)
   }
 
@@ -71,20 +70,84 @@ export default function UserDatabase({ firestore, users }) {
     setHovered(false)
   }
 
-  return (
-    <div className="user-database">
-      { users ? users.map(user => (
-        <UserCard key={user.uid}
-          user={user}
-          setSimpaticoMatch={setSimpaticoMatch}
-          getMatchBy={getMatchBy}
-          selectTheUser={selectTheUser}
-          selectedUser={selectedUser}
-          showSelectedUser={showSelectedUser}
-          hovered={hovered}
-          removeMatch={removeMatch} />
-      )) : <p>loading...</p>}
-    </div>
+  if (showAllUsers === true) {
+    return (
+      <div className="user-database">
+        {users ? users.map(user => (
+          <UserCard key={user.uid}
+            user={user}
+            setSimpaticoMatch={setSimpaticoMatch}
+            getMatchBy={getMatchBy}
+            selectTheUser={selectTheUser}
+            selectedUser={selectedUser}
+            showSelectedUser={showSelectedUser}
+            hovered={hovered}
+            removeMatch={removeMatch} />
+        )) : <p>loading...</p>}
+      </div>
+    );
+  }
 
-  );
+  if (showCauseMatches === true) {
+
+    return (
+      <div className="user-database-columns">
+        <div className='cause-column natural-causes'>
+          <h2>Natural</h2>
+          {users ? users.map(user => (
+            user.cause === 'Natural' ? <UserCard key={user.uid}
+              user={user}
+              setSimpaticoMatch={setSimpaticoMatch}
+              getMatchBy={getMatchBy}
+              selectTheUser={selectTheUser}
+              selectedUser={selectedUser}
+              showSelectedUser={showSelectedUser}
+              hovered={hovered}
+              removeMatch={removeMatch} /> : null
+          )) : <p>loading...</p>}
+        </div>
+        <div className='cause-column unnatural-causes'>
+          <h2>Unnatural</h2>
+          {users ? users.map(user => (
+            user.cause === 'Unnatural' ? <UserCard key={user.uid}
+              user={user}
+              setSimpaticoMatch={setSimpaticoMatch}
+              getMatchBy={getMatchBy}
+              selectTheUser={selectTheUser}
+              selectedUser={selectedUser}
+              showSelectedUser={showSelectedUser}
+              hovered={hovered}
+              removeMatch={removeMatch} /> : null
+          )) : <p>loading...</p>}
+
+        </div>
+      </div>
+    )
+
+  }
+
+  if (showKinshipMatches === true) {
+    return (
+      <div className="kinship-selections">
+        <div className='kinship-column'>
+          <button>Partner</button>
+          <button>Grandparent</button>
+          <button>Parent</button>
+          <button>Offspring</button>
+          <button>Sibling</button>
+          <button>Cousin</button>
+          <button>Grandchild</button>
+        </div>
+        <div className='kinship-column'>
+          <button>Aunt</button>
+          <button>Uncle</button>
+          <button>Niece</button>
+          <button>Nephew</button>
+          <button>Friend</button>
+          <button>Other</button>
+          <button>I want to support others</button>
+        </div>
+      </div>
+    )
+  }
 }
