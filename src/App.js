113ed24b2d9932f63b9c0995de63871f890497ea
mrjoemiller <joe@joemiller.co>
2024-01-30 09:52:38 -0500

    comments removed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { useState, useEffect } from 'react';
-// import { useAuthState } from 'react-firebase-hooks/auth';
 import Nav from './components/Nav/Nav';
 import Dashboard from './components/Dashboard/Dashboard';
 import UserAuth from './components/UserAuth/UserAuth';
-// import { firestore, auth } from './firebase-config';
 import { useAuth } from './components/UserAuth/AuthContext';
 
 function App() {
-  // const [user] = useAuthState(auth); // going to need a cognito switheroo here
   const { user, signIn, signOut } = useAuth();
   const [showWelcomeMessage, setShowWelcomeMessage] = useState(true);
   const [adminDash, setAdminDash] = useState(false);
   const [fsUser, setFsUser] = useState(null); // no more firestore, this is now a mysql stored user to sinc with the cognito user 
   const [fsMatch, setFsMatch] = useState(null); // lets rename to simpaticoMatch
   const [error, setError] = useState(null);
-
-  // const user = null
-
   //will need a useEffect to fetch the user, the user's conversations, and the user's contacts
 
 
@@ -24,7 +18,6 @@ function App() {
     if (renderCondition === 'welcome') {
       setShowWelcomeMessage(false);
     } else if (renderCondition === 'Logout') {
-      // auth.signOut();
       setShowWelcomeMessage(false);
       setFsUser(null);
       setFsMatch(null);
@@ -44,7 +37,7 @@ function App() {
               {error && <div>Error: {error.message}</div>}
 
               {user && (
-                // Render the Dashboard only if user is not null
+                // Render the Dashboard currently only if user is not null
                 <Dashboard
                   user={user}
                   match={fsMatch}
--- src/components/UserAuth/AuthContext.js
+++ src/components/UserAuth/AuthContext.js
@@ -5,6 +5,29 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
+    const exampleUser = {
+      "ethnicity": "Hispanic or Latino",
+      "lossExp": "loss exp",
+      "email": "joe@joemiller.co",
+      "simpaticoMatch": "lxKx8rN4tLcPXh8KO3cYyEOMEMt2",
+      "cause": "Natural",
+      "photoURL": "https://api.dicebear.com/5.x/thumbs/svg?seed=Casper",
+      "bioSex": "Female",
+      "education": "Primary school completed",
+      "hobbies": "hobbies",
+      "kinship": "Spouse",
+      "household": "Single parent with children",
+      "deceasedAge": "23",
+      "birthDate": "1980-01-09",
+      "uid": "C0OLyRdPvdWdPcoWUPCAgawogla2",
+      "residence": "Illinois",
+      "race": "Asian",
+      "displayName": "Joe Admin 1",
+      "lossDate": "1980-01-01",
+      "admin": true,
+      "consent": "false"
+    }
+
     const signIn = (username, password) => {
 
         setUser({ username });
